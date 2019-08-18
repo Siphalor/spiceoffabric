a69dabdc29d6a68b8340c319397b3d47fc4b2ed9
Siphalor <xyx0125@gmail.com>
2019-08-18 18:52:27 +0200

    Beta 9 of Version 1.2.0
    * Updated dependencies and Minecraft

--- src/main/java/de/siphalor/spiceoffabric/client/rei/REIPlugin.java
+++ src/main/java/de/siphalor/spiceoffabric/client/rei/REIPlugin.java
@@ -1,27 +1,34 @@
 package de.siphalor.spiceoffabric.client.rei;
 
 import de.siphalor.spiceoffabric.SpiceOfFabric;
-import me.shedaniel.rei.api.ItemRegistry;
-import me.shedaniel.rei.api.REIPluginEntry;
+import me.shedaniel.rei.api.EntryRegistry;
+import me.shedaniel.rei.api.plugins.REIPluginV0;
+import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.util.version.VersionParsingException;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Identifier;
 
-public class REIPlugin implements REIPluginEntry {
+public class REIPlugin implements REIPluginV0 {
 	@Override
 	public Identifier getPluginIdentifier() {
 		return new Identifier(SpiceOfFabric.MOD_ID, "rei_plugin");
 	}
 
 	@Override
-	public void registerItems(ItemRegistry itemRegistry) {
+	public void registerEntries(EntryRegistry entryRegistry) {
 		ItemStack itemStack = new ItemStack(Items.WRITTEN_BOOK);
 		CompoundTag compoundTag = itemStack.getOrCreateTag();
 		compoundTag.putString("title", "");
 		compoundTag.putString("author", "Me");
 		compoundTag.putBoolean(SpiceOfFabric.FOOD_JOURNAL_FLAG, true);
 		itemStack.getOrCreateSubTag("display").putString("Name", "{\"translate\":\"Diet Journal\",\"bold\":true}");
-		itemRegistry.registerItemStack(itemStack);
+		entryRegistry.registerItemStack(itemStack);
+	}
+
+	@Override
+	public SemanticVersion getMinimumVersion() throws VersionParsingException {
+		return SemanticVersion.parse("3.0-pre");
 	}
 }
