e81443c0511c2525eb9a6f8489ed49d375c9aa63
Siphalor <info@siphalor.de>
2020-03-21 14:10:48 +0100

    Fix eating being stuck

--- src/main/java/de/siphalor/spiceoffabric/mixin/MixinLivingEntity.java
+++ src/main/java/de/siphalor/spiceoffabric/mixin/MixinLivingEntity.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
@@ -27,10 +26,5 @@ public abstract class MixinLivingEntity {
 		if((Object) this instanceof PlayerEntity) {
 			MaxUseTimeCalculator.currentPlayer = (PlayerEntity)(Object) this;
 		}
-	}
-
-    @ModifyConstant(method = "tickActiveItemStack")
-	private int getConsumeEffectBeginTime(int old) {
-		return (int) (activeItemStack.getMaxUseTime() * 0.75F);
 	}
 }
