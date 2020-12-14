e5157632401c5ea25e95fa0ad3015164fc30bb87
Siphalor <info@siphalor.de>
2020-12-14 14:24:46 +0100

    Reindent file

--- src/main/java/de/siphalor/spiceoffabric/foodhistory/FoodHistory.java
+++ src/main/java/de/siphalor/spiceoffabric/foodhistory/FoodHistory.java
@@ -22,84 +22,84 @@ public class FoodHistory {
 
 	public Set<FoodHistoryEntry> carrotHistory;
 
-    public BiMap<Integer, FoodHistoryEntry> dictionary;
-    public int nextId = 0;
-    public Queue<Integer> history;
-    public Map<Integer, Integer> stats;
+	public BiMap<Integer, FoodHistoryEntry> dictionary;
+	public int nextId = 0;
+	public Queue<Integer> history;
+	public Map<Integer, Integer> stats;
 
-    public FoodHistory() {
-    	setup();
-    }
+	public FoodHistory() {
+		setup();
+	}
 
-    public void setup() {
-    	dictionary = HashBiMap.create();
-    	history = new ConcurrentLinkedQueue<>();
-    	stats = new Int2IntArrayMap();
-    	if(Config.carrot.enable)
-    		carrotHistory = new HashSet<>();
-    }
+	public void setup() {
+		dictionary = HashBiMap.create();
+		history = new ConcurrentLinkedQueue<>();
+		stats = new Int2IntArrayMap();
+		if (Config.carrot.enable)
+			carrotHistory = new HashSet<>();
+	}
 
-    public void reset() {
-    	dictionary.clear();
-    	nextId = 0;
-    	history.clear();
-    	stats.clear();
-    	if(carrotHistory != null) carrotHistory.clear();
+	public void reset() {
+		dictionary.clear();
+		nextId = 0;
+		history.clear();
+		stats.clear();
+		if (carrotHistory != null) carrotHistory.clear();
 	}
 
 	public void write(PacketByteBuf buffer) {
-    	buffer.writeVarInt(dictionary.size());
-    	for(Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
-    		buffer.writeVarInt(entry.getKey());
-    		entry.getValue().write(buffer);
+		buffer.writeVarInt(dictionary.size());
+		for (Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
+			buffer.writeVarInt(entry.getKey());
+			entry.getValue().write(buffer);
 		}
-        buffer.writeVarInt(history.size());
-    	for(int integer : history) {
-    		buffer.writeVarInt(integer);
+		buffer.writeVarInt(history.size());
+		for (int integer : history) {
+			buffer.writeVarInt(integer);
 		}
-    	if(carrotHistory != null) {
-    		buffer.writeBoolean(true);
-    		buffer.writeVarInt(carrotHistory.size());
-    		for(FoodHistoryEntry entry : carrotHistory) {
-    			entry.write(buffer);
+		if (carrotHistory != null) {
+			buffer.writeBoolean(true);
+			buffer.writeVarInt(carrotHistory.size());
+			for (FoodHistoryEntry entry : carrotHistory) {
+				entry.write(buffer);
 			}
 		} else
 			buffer.writeBoolean(false);
 	}
 
 	public void read(PacketByteBuf buffer) {
-    	for(int l = buffer.readVarInt(), i = 0; i < l; i++) {
-    		dictionary.put(buffer.readVarInt(), FoodHistoryEntry.from(buffer));
+		for (int l = buffer.readVarInt(), i = 0; i < l; i++) {
+			dictionary.put(buffer.readVarInt(), FoodHistoryEntry.from(buffer));
 		}
-    	for(int l = buffer.readVarInt(), i = 0; i < l; i++) {
-    		history.offer(buffer.readVarInt());
+		for (int l = buffer.readVarInt(), i = 0; i < l; i++) {
+			history.offer(buffer.readVarInt());
 		}
-    	if(buffer.readBoolean()) {
-    		final int length = buffer.readVarInt();
-    		carrotHistory = new HashSet<>(length);
-    		for(int i = 0; i < length; i++) {
-    			carrotHistory.add(FoodHistoryEntry.from(buffer));
+		if (buffer.readBoolean()) {
+			final int length = buffer.readVarInt();
+			carrotHistory = new HashSet<>(length);
+			for (int i = 0; i < length; i++) {
+				carrotHistory.add(FoodHistoryEntry.from(buffer));
 			}
 		}
-    	buildStats();
+		buildStats();
 	}
 
 	public CompoundTag write(CompoundTag compoundTag) {
 		defragmentDictionary();
 		ListTag list = new ListTag();
-		for(Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
+		for (Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
 			list.add(entry.getKey(), entry.getValue().write(new CompoundTag()));
 		}
 		compoundTag.put("dictionary", list);
 		ListTag historyList = new ListTag();
-		for(Integer id : history) {
+		for (Integer id : history) {
 			historyList.add(IntTag.of(id));
 		}
 		compoundTag.put("history", historyList);
-		if(carrotHistory != null) {
+		if (carrotHistory != null) {
 			ListTag carrotHistoryList = new ListTag();
-			for(FoodHistoryEntry entry : carrotHistory) {
-                carrotHistoryList.add(entry.write(new CompoundTag()));
+			for (FoodHistoryEntry entry : carrotHistory) {
+				carrotHistoryList.add(entry.write(new CompoundTag()));
 			}
 			compoundTag.put("carrotHistory", carrotHistoryList);
 		}
@@ -107,22 +107,22 @@ public class FoodHistory {
 	}
 
 	public static FoodHistory read(CompoundTag compoundTag) {
-        FoodHistory foodHistory = new FoodHistory();
+		FoodHistory foodHistory = new FoodHistory();
 		ListTag list = (ListTag) compoundTag.get("dictionary");
-        for(int i = 0; i < list.size(); i++) {
-        	foodHistory.dictionary.put(i, new FoodHistoryEntry().read(list.getCompound(i)));
-        }
-        foodHistory.nextId = foodHistory.dictionary.size();
-        list = (ListTag) compoundTag.get("history");
-        for(Tag tag : list) {
-        	foodHistory.history.offer(((IntTag) tag).getInt());
-        }
-        foodHistory.buildStats();
+		for (int i = 0; i < list.size(); i++) {
+			foodHistory.dictionary.put(i, new FoodHistoryEntry().read(list.getCompound(i)));
+		}
+		foodHistory.nextId = foodHistory.dictionary.size();
+		list = (ListTag) compoundTag.get("history");
+		for (Tag tag : list) {
+			foodHistory.history.offer(((IntTag) tag).getInt());
+		}
+		foodHistory.buildStats();
 
-        if(compoundTag.contains("carrotHistory")) {
-        	list = (ListTag) compoundTag.get("carrotHistory");
-        	if(Config.carrot.enable) {
-        		foodHistory.carrotHistory = new HashSet<>();
+		if (compoundTag.contains("carrotHistory")) {
+			list = (ListTag) compoundTag.get("carrotHistory");
+			if (Config.carrot.enable) {
+				foodHistory.carrotHistory = new HashSet<>();
 				for (Tag tag : list) {
 					foodHistory.carrotHistory.add(new FoodHistoryEntry().read((CompoundTag) tag));
 				}
@@ -132,73 +132,72 @@ public class FoodHistory {
 	}
 
 	public void buildStats() {
-    	stats.clear();
-    	for(Integer id : history) {
-    		if(stats.containsKey(id)) {
+		stats.clear();
+		for (Integer id : history) {
+			if (stats.containsKey(id)) {
 				stats.put(id, stats.get(id) + 1);
-			}
-    		else {
+			} else {
 				stats.put(id, 1);
 			}
-	    }
+		}
 	}
 
 	public void defragmentDictionary() {
 		Map<Integer, Integer> oldToNewMap = new HashMap<>();
 		int i = 0;
-        for(Integer id : dictionary.keySet()) {
-        	oldToNewMap.put(id, i);
-        	i++;
-        }
-        nextId = i;
-        Queue<Integer> newHistory = new ConcurrentLinkedQueue<>();
-        while(true) {
-        	Integer id = history.poll();
-        	if(id == null) break;
-        	newHistory.offer(id);
-        }
-        history = newHistory;
-        Map<Integer, Integer> newStats = new Int2IntArrayMap();
-        for(Map.Entry<Integer, Integer> entry : stats.entrySet()) {
-        	newStats.put(oldToNewMap.get(entry.getKey()), entry.getValue());
-        }
-        stats = newStats;
-        BiMap<Integer, FoodHistoryEntry> newDictionary = HashBiMap.create();
-        for(Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
-        	newDictionary.put(oldToNewMap.get(entry.getKey()), entry.getValue());
-        }
-        dictionary = newDictionary;
+		for (Integer id : dictionary.keySet()) {
+			oldToNewMap.put(id, i);
+			i++;
+		}
+		nextId = i;
+		Queue<Integer> newHistory = new ConcurrentLinkedQueue<>();
+		while (true) {
+			Integer id = history.poll();
+			if (id == null) break;
+			newHistory.offer(id);
+		}
+		history = newHistory;
+		Map<Integer, Integer> newStats = new Int2IntArrayMap();
+		for (Map.Entry<Integer, Integer> entry : stats.entrySet()) {
+			newStats.put(oldToNewMap.get(entry.getKey()), entry.getValue());
+		}
+		stats = newStats;
+		BiMap<Integer, FoodHistoryEntry> newDictionary = HashBiMap.create();
+		for (Map.Entry<Integer, FoodHistoryEntry> entry : dictionary.entrySet()) {
+			newDictionary.put(oldToNewMap.get(entry.getKey()), entry.getValue());
+		}
+		dictionary = newDictionary;
 	}
 
 	public int getTimesEaten(ItemStack stack) {
-    	return stats.getOrDefault(dictionary.inverse().get(FoodHistoryEntry.fromItemStack(stack)), 0);
+		return stats.getOrDefault(dictionary.inverse().get(FoodHistoryEntry.fromItemStack(stack)), 0);
 	}
 
 	public void addFood(ItemStack stack, ServerPlayerEntity serverPlayerEntity) {
-    	FoodHistoryEntry entry = FoodHistoryEntry.fromItemStack(stack);
+		FoodHistoryEntry entry = FoodHistoryEntry.fromItemStack(stack);
 
 		if (ServerPlayNetworking.canSend(serverPlayerEntity, SpiceOfFabric.ADD_FOOD_S2C_PACKET)) {
 			PacketByteBuf buffer = new PacketByteBuf(Unpooled.buffer());
 			entry.write(buffer);
 			ServerPlayNetworking.send(serverPlayerEntity, SpiceOfFabric.ADD_FOOD_S2C_PACKET, buffer);
 		}
-    	addFood(entry);
-    }
+		addFood(entry);
+	}
 
-    public void addFood(FoodHistoryEntry entry) {
+	public void addFood(FoodHistoryEntry entry) {
 		Integer id = dictionary.inverse().get(entry);
-		if(id == null) {
+		if (id == null) {
 			id = nextId++;
 			dictionary.put(id, entry);
 		}
 		history.offer(id);
-		while(history.size() > Config.food.historyLength) {
+		while (history.size() > Config.food.historyLength) {
 			removeLastFood();
 		}
 		stats.put(id, stats.getOrDefault(id, 0) + 1);
 
-		if(Config.carrot.enable) {
-			if(carrotHistory == null) {
+		if (Config.carrot.enable) {
+			if (carrotHistory == null) {
 				System.err.println("Carrot food history is null");
 			} else {
 				carrotHistory.add(entry);
@@ -206,17 +205,17 @@ public class FoodHistory {
 		}
 	}
 
-    public void removeLastFood() {
-    	int id = history.remove();
-    	if (stats.containsKey(id)) stats.put(id, stats.get(id) - 1);
-    }
+	public void removeLastFood() {
+		int id = history.remove();
+		if (stats.containsKey(id)) stats.put(id, stats.get(id) - 1);
+	}
 
-    public ListTag genJournalPages(PlayerEntity playerEntity) {
-    	boolean hasMod = ((IServerPlayerEntity) playerEntity).spiceOfFabric_hasClientMod();
+	public ListTag genJournalPages(PlayerEntity playerEntity) {
+		boolean hasMod = ServerPlayNetworking.canSend((ServerPlayerEntity) playerEntity, SpiceOfFabric.ADD_FOOD_S2C_PACKET);
 
-    	ListTag pages = new ListTag();
+		ListTag pages = new ListTag();
 
-    	LiteralText textOnPage = new LiteralText("");
+		LiteralText textOnPage = new LiteralText("");
 		textOnPage.append(
 				hasMod
 						? new TranslatableText(SpiceOfFabric.MOD_ID + ".journal.inside_title")
@@ -234,9 +233,9 @@ public class FoodHistory {
 
 		int linesOnPage = 2;
 		int number = 1;
-		for(int foodId : history) {
+		for (int foodId : history) {
 			FoodHistoryEntry entry = dictionary.get(foodId);
-			if(hasMod) {
+			if (hasMod) {
 				textOnPage.append(
 						new TranslatableText(
 								SpiceOfFabric.MOD_ID + ".journal.line",
@@ -249,24 +248,25 @@ public class FoodHistory {
 						.append("\n");
 			} else {
 				textOnPage.append(new LiteralText(number + ". ").setStyle(numberStyle))
-						.append(entry.getStackName().setStyle(itemStyle.withHoverEvent(
+						.append(entry.getStackName().setStyle(
+								itemStyle.withHoverEvent(
 										new HoverEvent(
 												HoverEvent.Action.SHOW_ITEM,
 												new HoverEvent.ItemStackContent(entry.getStack())
 										)
-								).withBold(false)
-						)
-				).append("\n");
+								).withBold(false))
+						).append("\n");
 			}
-			number++; linesOnPage++;
-			if(linesOnPage >= 14) {
+			number++;
+			linesOnPage++;
+			if (linesOnPage >= 14) {
 				pages.add(StringTag.of(Text.Serializer.toJson(textOnPage)));
 				linesOnPage = 0;
 				textOnPage = new LiteralText("");
 			}
 		}
 
-		if(linesOnPage > 0) {
+		if (linesOnPage > 0) {
 			pages.add(StringTag.of(Text.Serializer.toJson(textOnPage)));
 		}
 
