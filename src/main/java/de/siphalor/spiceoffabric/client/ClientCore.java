6fb5dbf8145b40e6d46f39f9ca4630e7f3475626
Siphalor <info@siphalor.de>
2021-01-31 17:12:18 +0100

    Fix #18 (Syncing the food history too early causing errors)

--- src/main/java/de/siphalor/spiceoffabric/client/ClientCore.java
+++ src/main/java/de/siphalor/spiceoffabric/client/ClientCore.java
@@ -4,14 +4,30 @@ import de.siphalor.spiceoffabric.SpiceOfFabric;
 import de.siphalor.spiceoffabric.foodhistory.FoodHistoryEntry;
 import de.siphalor.spiceoffabric.util.IHungerManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
 
 public class ClientCore implements ClientModInitializer {
+	private static PacketByteBuf lastSyncPacket;
+
 	@Override
 	public void onInitializeClient() {
-		ClientPlayNetworking.registerGlobalReceiver(SpiceOfFabric.SYNC_FOOD_HISTORY_S2C_PACKET, (client, handler, buf, responseSender) ->
-				((IHungerManager) client.player.getHungerManager()).spiceOfFabric_getFoodHistory().read(buf)
-		);
+		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+			if (lastSyncPacket != null) {
+				((IHungerManager) client.player.getHungerManager()).spiceOfFabric_getFoodHistory().read(lastSyncPacket);
+				lastSyncPacket.release();
+				lastSyncPacket = null;
+			}
+		});
+		ClientPlayNetworking.registerGlobalReceiver(SpiceOfFabric.SYNC_FOOD_HISTORY_S2C_PACKET, (client, handler, buf, responseSender) -> {
+			if (client.player != null && client.player.getHungerManager() != null) {
+				((IHungerManager) client.player.getHungerManager()).spiceOfFabric_getFoodHistory().read(buf);
+			} else {
+				lastSyncPacket = new PacketByteBuf(buf.copy());
+				lastSyncPacket.retain();
+			}
+		});
 		ClientPlayNetworking.registerGlobalReceiver(SpiceOfFabric.ADD_FOOD_S2C_PACKET, (client, handler, buf, responseSender) ->
 				((IHungerManager) client.player.getHungerManager()).spiceOfFabric_getFoodHistory().addFood(FoodHistoryEntry.from(buf))
 		);
