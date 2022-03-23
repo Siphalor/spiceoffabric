4454b7f3f1c1cac4a020bed2dea99849edc87057
Siphalor <info@siphalor.de>
2022-03-23 20:59:58 +0100

    Rename ClientCore to SoFClient

--- src/main/java/de/siphalor/spiceoffabric/client/SoFClient.java
+++ src/main/java/de/siphalor/spiceoffabric/client/SoFClient.java
@@ -1,17 +1,14 @@
 package de.siphalor.spiceoffabric.client;
 
 import de.siphalor.spiceoffabric.SpiceOfFabric;
-import de.siphalor.spiceoffabric.config.Config;
 import de.siphalor.spiceoffabric.foodhistory.FoodHistoryEntry;
 import de.siphalor.spiceoffabric.util.IHungerManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
-import squeek.appleskin.api.event.FoodValuesEvent;
-import squeek.appleskin.api.food.FoodValues;
 
-public class ClientCore implements ClientModInitializer {
+public class SoFClient implements ClientModInitializer {
 	private static PacketByteBuf lastSyncPacket;
 
 	@Override
